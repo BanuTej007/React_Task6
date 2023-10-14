057ebabaca716d71913f8bb366e893a6840cb6f5
BanuTej007 <144442282+BanuTej007@users.noreply.github.com>
2023-10-14 23:00:58 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import './nav.css'
 function Nav(){
     return(
         <nav class="navbar p-5 name">
-            <Link to="/" class="navbar-brand text-light"><h2>Banu Tej</h2></Link>
+            <Link to="/" class="navbar-brand text-light"><h2>Maram Banu Teja</h2></Link>
             <div class="nav link">
             <Link to="/" class="nav-link name"><p>Home</p></Link>
             <Link to="/about" class="nav-link"><p>About</p></Link>
